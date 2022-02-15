1a4bfab9c5dbab5e6b18d383a43a91f76801ae19
szq <1056628965@qq.com>
2022-02-15 18:55:42 +0800

    feat: 路由切换滚动问题

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,12 @@ import routes from "./config";
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
+  scrollBehavior() {
+    // 由于外部容器固定了高度 实际上滚动的只有el-main容器的内容 故用js自己处理局部滚动到顶部
+    document.getElementsByClassName("el-main")[0].scrollTop = 0;
+    // 页面始终滚动到顶部
+    return { x: 0, y: 0 };
+  },
 });
 
 export default router;
