1f1aaeb9cc7a9f4d8fab9bfa1250f0392a5b233b
szq <1056628965@qq.com>
2022-02-18 21:04:48 +0800

    feat: 国际化设置

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import "./index.less";
+// import locale from "element-ui/lib/locale/lang/en";
 
+// 国际化设置
+// Vue.use(ElementUI, { locale });
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
