8b019dc1483155cd907fdd7e84459ca199b19764
szq <1056628965@qq.com>
2022-03-27 19:02:14 +0800

    feat

--- src/views/mode/index.vue
+++ src/views/mode/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <p>
+      参考vue-cli<a
+        href="https://cli.vuejs.org/zh/guide/mode-and-env.html"
+        target="_blank"
+        >模式和环境变量</a
+      >进行配置，vue-admin中演示实际开发中常用到自定义环境变量区分本地开发dev环境，qa环境，预发pre环境和线上生产prod环境
+    </p>
     <p>process.env.NODE_ENV：{{ nodeEnv }}</p>
     <p>process.env.VUE_APP_ENV：{{ vueAppEnv }}</p>
   </div>
